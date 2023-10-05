33b22479d31a26e31ac3232b0ead8eb7a968acf1
N-eeraj <neeraj7rajeena@gmail.com>
2023-10-06 00:58:28 +0530

    Home screen design Added custom button component

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -1,21 +1,28 @@
 import { useContext } from 'react'
 
+import Button from '@components/Button'
 import { ScreenContext } from '@/App'
 
 const Home = () => {
   const { setScreen } = useContext(ScreenContext)
 
   return (
-    <div>
-      <button onClick={() => setScreen('singlePlayer')}>
-        Single Player
-      </button>
-      <button onClick={() => setScreen('multiPlayer')}>
-        Multi Player
-      </button>
-      <button onClick={() => setScreen('rules')}>
-        Rules
-      </button>
+    <div className='flex flex-col justify-center items-center gap-y-20 md:gap-y-10 w-full h-full px-10 bg-gradient-to-b from-accent to-primary'>
+      <h1 className='text-white text-6xl text-center leading-tight'>
+        Elemental Fight
+      </h1>
+
+      <div className='flex flex-col gap-y-6 w-full max-w-xs'>
+        <Button onClick={() => setScreen('singlePlayer')}>
+          Single Player
+        </Button>
+        <Button onClick={() => setScreen('multiPlayer')}>
+          Multi Player
+        </Button>
+        <Button onClick={() => setScreen('rules')}>
+          Rules
+        </Button>
+      </div>
     </div>
   )
 }
