cbf7468cab3cfd430d8207856c41b9f32e5d76a8
N-eeraj <neeraj7rajeena@gmail.com>
2023-10-06 00:58:28 +0530

    Home screen design Added custom button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ onClick, children }) => {
+  return (
+    <button className='group relative' onClick={onClick}>
+      <div className='w-full p-5 bg-white text-primary text-3xl group-active:translate-x-1 group-active:translate-y-1 duration-300'>
+        {children}
+      </div>
+      <div className='absolute top-0 left-full w-2 h-full bg-gray-200 skew-y-[45deg] origin-top-left group-active:w-0 group-active:opacity-0 duration-300' />
+      <div className='absolute top-full left-0 w-full h-2 bg-gray-400 skew-x-[45deg] origin-bottom-right translate-x-2 group-active:h-0 group-active:opacity-0 duration-300' />
+    </button>
+  )
+}
+
+export default Button
