854d19c13cf28aa5ec710209e351ee0b20266cc6
N-eeraj <neeraj7rajeena@gmail.com>
2023-10-10 09:17:57 +0530

    Added react icons & rules page setup

--- src/components/screens/Rules.jsx
+++ src/components/screens/Rules.jsx
@@ -1,7 +1,18 @@
+import { IoClose } from 'react-icons/io5'
+
 const Rules = () => {
   return (
-    <div className='magestic-screen'>
-      
+    <div className='flex flex-col gap-y-8 magestic-screen py-6'>
+      <div className='relative flex justify-center items-center w-full'>
+        <IoClose className='absolute left-0 text-white text-6xl' />
+        <h2 className='text-white text-6xl md:text-6xl text-center font-title leading-tight'>
+          Rules
+        </h2>
+      </div>
+
+      <div className='h-full px-10 overflow-y-auto text-white'>
+        
+      </div>
     </div>
   )
 }
