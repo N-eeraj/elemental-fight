cff6a6118788d51be7e957c9e19af5c3709d9a09
N-eeraj <neeraj7rajeena@gmail.com>
2023-10-11 15:21:07 +0530

    Added rules

--- src/components/screens/Rules.jsx
+++ src/components/screens/Rules.jsx
@@ -1,17 +1,42 @@
+import { useContext } from 'react'
 import { IoClose } from 'react-icons/io5'
+import { ScreenContext } from '@/App'
 
 const Rules = () => {
+  const { setScreen } = useContext(ScreenContext)
+
   return (
-    <div className='flex flex-col gap-y-8 magestic-screen py-6'>
+    <div className='flex flex-col gap-y-8 magestic-screen py-6 md:px-9'>
       <div className='relative flex justify-center items-center w-full'>
-        <IoClose className='absolute left-0 text-white text-6xl' />
+        <button className='group absolute left-0' onClick={() => setScreen('home')}>
+          <IoClose className='text-white text-5xl duration-200 group-hover:rotate-90' />
+        </button>
+
         <h2 className='text-white text-6xl md:text-6xl text-center font-title leading-tight'>
           Rules
         </h2>
       </div>
 
-      <div className='h-full px-10 overflow-y-auto text-white'>
-        
+      <div className='h-full overflow-y-auto text-white'>
+        <ul className='flex flex-col gap-y-5 w-full px-10 list-disc text-xl md:text-2xl font-body'>
+          <li>
+            Elemental Fight is a game similar to rock paper scissors.
+          </li>
+          <li>
+            You need to choose among 5 elements; Fire, Grass, Water, Lightning & Rock.
+            <br />
+            Your opponent will also choose one.
+          </li>
+          <li>
+            Each element is capable of defeating 2 elements & will be defeated by the other 2.
+          </li>
+          <li>
+            Your score one point on beating the opponent's element of each round.
+          </li>
+          <li>
+            First to score 3 points win.
+          </li>
+        </ul>
       </div>
     </div>
   )
